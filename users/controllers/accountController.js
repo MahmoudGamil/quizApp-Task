c5aa9be7ef16706466c12cf7ae3b009a11e86053
MahmoudGamil <30590010+MahmoudGamil@users.noreply.github.com>
2017-08-03 19:44:12 +0200

    users

---
+++ users/controllers/accountController.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    angular.module("quizApp")
+    .controller("accountController", function ($scope, $http, $rootScope, $location ) {
+        var vm = this;
+        vm.email = "";
+        vm.password = "";
+        vm.login = login;
+        vm.failedToLogin = false;
+        vm.isLoggedin = false;
+        activate();
+
+
+        function activate() {
+            if ($rootScope.isLoggedin) {
+                $location.path('/questions');
+            }
+        }
+
+         function login( ) {
+             $http.get('/database/users.json').then(function (response) {
+                 for (var i = 0; i < response.data.users.length; i++)
+                     if (response.data.users[i].email == vm.email &&
+                         response.data.users[i].password == vm.password) {
+                         $rootScope.isLoggedin = true;
+                         $location.path('/questions');
+
+                     } else {
+                         vm.failedToLogin = true;
+                     }
+             },
+                function error(e) {
+                    alert(e.message);
+                }
+                    
+
+
+            )
+        }
+    })
+})();
