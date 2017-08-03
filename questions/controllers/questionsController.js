7172dcf101ecdb68ee6b4c038b672f8416a3a675
MahmoudGamil <30590010+MahmoudGamil@users.noreply.github.com>
2017-08-03 19:32:44 +0200

    questions

---
+++ questions/controllers/questionsController.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    angular.module("quizApp")
+    .controller("questionsController", function ($scope, $http, $location, $rootScope) {
+        var vm = this;
+        vm.questions = [];
+        vm.selectedAnswer = [];
+        vm.submit = submit;
+        vm.activate = activate;
+        vm.result = 0;
+        vm.isSubmitted = false;
+
+        activate();
+        function activate() {
+            if (!$rootScope.isLoggedin) {
+                $location.path('/login');
+            }
+            $http.get('/database/questionsAndAnswers.json')
+            .then(function (response) {
+              
+                vm.questions = response.data.questions;
+            }, 
+            function error(e) {
+                alert(e.message);
+            })  
+        }
+        function submit() {
+            vm.isSubmitted = true;
+            for (var i = 0; i < vm.questions.length; i++) {
+                if (vm.selectedAnswer[i] == vm.questions[i].correctAnswer) {
+                    vm.result++;
+                    
+                }
+            }
+        }
+
+    })
+})();
