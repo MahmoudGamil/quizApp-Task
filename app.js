4dcbf9b09757527ac56168b97dcd4f975bbc24f7
MahmoudGamil <30590010+MahmoudGamil@users.noreply.github.com>
2017-08-03 19:44:58 +0200

    main

---
+++ app.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    var app = angular.module("quizApp", ['ngRoute']);
+    app.config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+        .when('/login', {
+            templateUrl: 'users/templates/login.html',
+            controller: 'accountController',
+            controllerAs: 'vm'  
+           }).when('/questions', {
+               templateUrl: 'questions/templates/questions.html',
+               controller: 'questionsController',
+               controllerAs: 'vm'
+
+        }).when('/resultsAndAnswes', {
+            templateUrl: 'questions/templates/resultsAndAnswers.html',
+            controller: 'questionsController',
+            controllerAs: 'vm'  
+
+        }).otherwise({ redirectTo: '/login' });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+       
+       
+    })
+    // hww enta 7adretak 7atet om el controller gwwa el app.config??
+    app.controller("mainController", mainController);
+    function mainController($rootScope, $location) {
+        // om el main controller
+        var vm = this;
+        vm.logout = logout;
+
+        function logout() {
+            $rootScope.isLoggin = false;
+            $location.path('/login');
+        }
+    }
+})();
+
